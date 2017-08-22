22ed1fada552809d60298073b4a1c435a670f88d
JamieMaple <jamiemaple007@gmail.com>
2017-08-22 10:49:24 +0800

    reconstruction and aja

--- demo/ajax/ajax.js
+++ demo/ajax/ajax.js
@@ -1,121 +1,91 @@
-function jamieAjax(options){
-  'use strict'
-
-  let url = options.url || '',                            // request url set
-      type = (options.type || "get").toLowerCase(),       // request type, defalt : get
-      params = options.params || null,                          // request data
-      contentType = options.contentType || '',            // request header set
-      dataType = options.dataType || '',                  // request type set
-      async = options.async || true,                      // request async
-      timeOut = options.timeOut,                          // request time out
-      before = options.before || (() => {}),              // before send        
-      error = options.error || (() => {}),                // error handle
-      success = options.success || (() => {})             // sucess handle
-  
-  if (params && params instanceof Object) {
-    for(key in params) {
-
+const ajaxObj = {
+  _default: {
+    url  : '',
+    data : null,
+    contentType: 'application/x-www-form-urlencoded',
+    callback(data){},
+    async : true
+  },
+  _init(params) {
+    return Object.assign({}, this._default, params)
+  },
+  _request() {
+    // 兼容性问题
+    if (window.XMLHttpRequest) {
+      return new XMLHttpRequest()
     }
-  }
-
-  if (type === 'jsonp') {
-    createJSONP()
-  } else {
-    createXML()
-  }
-    
-
-  //编码数据
-  function setData() {
-    //设置对象的遍码
-      function setObjData(data, parentName) {
-          function encodeData(name, value, parentName) {
-              var items = [];
-              name = parentName === undefined ? name : parentName + "[" + name + "]";
-              if (typeof value === "object" && value !== null) {
-                  items = items.concat(setObjData(value, name));
-              } else {
-                  name = encodeURIComponent(name);
-                  value = encodeURIComponent(value);
-                  items.push(name + "=" + value);
-              }
-              return items;
-          }
-          var arr = [],value;
-          if (Object.prototype.toString.call(data) == '[object Array]') {
-              for (var i = 0, len = data.length; i < len; i++) {
-                  value = data[i];
-                  arr = arr.concat(encodeData( typeof value == "object"?i:"", value, parentName));
-              }
-          } else if (Object.prototype.toString.call(data) == '[object Object]') {
-              for (var key in data) {
-                  value = data[key];
-                  arr = arr.concat(encodeData(key, value, parentName));
-              }
-          }
-          return arr;
-      };
-      //设置字符串的遍码，字符串的格式为：a=1&b=2;
-      function setStrData(data) {
-          var arr = data.split("&");
-          for (var i = 0, len = arr.length; i < len; i++) {
-              name = encodeURIComponent(arr[i].split("=")[0]);
-              value = encodeURIComponent(arr[i].split("=")[1]);
-              arr[i] = name + "=" + value;
-          }
-          return arr;
-      }
-
-      if (data) {
-          if (typeof data === "string") {
-              data = setStrData(data);
-          } else if (typeof data === "object") {
-              data = setObjData(data);
-          }
-          data = data.join("&").replace("/%20/g", "+");
-          //若是使用get方法或JSONP，则手动添加到URL中
-          if (type === "get" || dataType === "jsonp") {
-              url += url.indexOf("?") > -1 ? (url.indexOf("=") > -1 ? "&" + data : data) : "?" + data;
-          }
-      }
-  }
-  function createXML() {
-    // traditional compatible ie6 <=
-    function getXHR() {
-      if (window.XMLHttpRequest) {
-        return new XMLHttpRequest()
-      }else {
-        let versions = ["Microsoft", "msxm3", "msxml2", "msxml1"]
-        for (let i = 0, length = versions.length; i < length; i++) {
-          let version = versions[i]+'.XMLHTTP'
-          return ActiveXObject(version)
-        }
-      }
+    const versions = [
+      "MSXML2.XmlHttp.6.0",
+      "MSXML2.XmlHttp.5.0",
+      "MSXML2.XmlHttp.4.0",
+      "MSXML2.XmlHttp.3.0",
+      "MSXML2.XmlHttp.2.0",
+      "Microsoft.XmlHttp"
+    ]
+    let xhr
+    for (let i = 0, length = versions.length; i < length; i++) {
+      try {
+        xhr = new ActiveXObject(versions[i])
+        break;
+      }catch(e){}
     }
-    // 惰性函数处理兼容性
-    var xhr = getXHR()
-    xhr.open(type, url, async)
-    xhr.onreadyStateChange = function() {
+    return xhr
+  },
+  _objToString(data) {
+    let arr = []
+    for (key in data){
+      arr.push(`${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
+      // q1 arr.push(`${key}=${data[key]}`)
+    }
+    return arr.join('&')
+  },
+  _send(type, params) {
+    let xml = this._request()
+    xml.open(type, params.url, params.async)
+    xml.setRequestHeader('Content-type', params.contentType)
+    xml.onreadystatechange = function() {
       if (this.readyState === 4) {
-        if ((this.status >= 200 && this.status < 300) || this.status === 304) {
-          success(this.responseText)
-        } else {
-          error(this.status, this.statusText)
-        }
+        params.callback(this.responseText)
       }
     }
-    xhr.send(type === 'get'? null : params)
-  }
-  function createJSONP() {
+    xml.send(params.data)
+  },
+  post(params) {
+    params.data = this._objToString(params.data)
+    params = this._init(params)
+    this._send('post', params)
+  },
+  get(params) {
+    let url_query = this._objToString(params.data)
+    if (url_query) {
+      params.url += '?'+url_query
+    } 
+    params.data = null
+
+    params = this._init(params)
+    this._send('get', params)
+  },
+  jsonp(params) {
+    
     let script = document.createElement('script'),
-        random_part = (Math.random()*1000),
-        callback = '_callback'+new Date().getTime()+Math.round(Math.random()*10000000)
+        url_query = this._objToString(params.data)
+        callback = 'JSONP'+new Date().getMinutes()
+    if (url_query) {
+      params.url += '?'+url_query
+      params.url += '&callback='+callback
+    } else {
+      params.url +='?callback='+callback
+    }
+    
     window[callback] = (data) => {
       document.head.removeChild(script)
-      success(data)
+      params.callback(data)
     }
-    script.src = url + (url.indexOf('?') > -1? '&' : "?") + 'callback='+callback
-    script.type= 'text/javascript'
+    if (!params.catch) {
+      params.url += '&_='+new Date().getTime() + Math.round(Math.random()*10000)
+    } 
+
+    script.src=params.url
     document.head.appendChild(script)
   }
 }
--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const APP_PORT = 3000
 
+app.use(bodyParser.json({limit: '1mb'}))    //body-parser 解析json格式数据
+app.use(bodyParser.urlencoded({            //此项必须在 bodyParser.json 下面,为参数编码
+  extended: true
+}))
 app.use('/demo', express.static('demo'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'demo', 'index.html'))
+})
+app.post('/test', (req, res) => {
+  res.send({"success":"post-test"})
+})
+app.get('/test', (req, res) => {
+  res.send("success")
 })
 
 app.listen(APP_PORT, function(req, res) {
